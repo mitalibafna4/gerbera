53bcf2dfc8e31639c3aab1850a061f29c84664e1
Mitali Bafna <80626622+mitalibafna4@users.noreply.github.com>
2024-03-12 23:42:38 +0530

    Add files via upload

---
+++ pages/1_About.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.write("""
+Hello, we are group no. 15 of KKWIEER
+""")
+
+st.write("# How to Use this App:")
+
+st.write("""
+##### Go to 3rd step
+""")
+
+st.write("""
+#### Okay
+""")
+
+st.write("""
+### Thank you
+""")
